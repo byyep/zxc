b08264e0bdacb3741328d99ad9eba77fae735920
byyep <bulahovskiy2@gmail.com>
2021-07-13 22:15:04 +0600

    1st

--- index/index/Program.cs
+++ index/index/Program.cs
@@ -7,9 +7,36 @@ namespace index
     {
         static void Main(string[] args)
         {
-            string str = "Hello World! =)";
+            vehicle your_car = new vehicle(10, 15, 20);
+            your_car.characteristic();
+        }
+    }
+    public class vehicle
+    {
+        protected uint gas_volume { get; set; }
+        protected uint number_of_wheels { get; set; }
+        protected uint size { get; set; }
+        public vehicle(uint gas_volume, uint number_of_wheels, uint size)
+        {
+            this.gas_volume = gas_volume;
+            this.number_of_wheels = number_of_wheels;
+            this.size = size; 
+        } 
+        public void characteristic()
+        {
+            Console.WriteLine("Объем бака: " + gas_volume 
+                + "\nКоличество колес: " + number_of_wheels + "\nГабариты: " + size);
+        }
 
-            Console.WriteLine(str[0..5]); 
+    }
+    public class car : vehicle
+    {
+        public car(uint gas_volume, uint number_of_wheels, uint size) 
+            : base(gas_volume, number_of_wheels, size)
+        {
+            this.gas_volume = gas_volume;
+            this.number_of_wheels = number_of_wheels;
+            this.size = size;
         }
     }
 }
