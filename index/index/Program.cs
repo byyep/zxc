b4cada499569f0e8205090f5f2e97e86e23b194e
byyep <bulahovskiy2@gmail.com>
2021-07-13 20:42:03 +0600

    Добавил index

---
+++ index/index/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq; 
+
+namespace index
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello World! =)";
+
+            Console.WriteLine(str[0..5]); 
+        }
+    }
+}
